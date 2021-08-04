cce5bfa6153557113deb96ca9ded3d50cfae20e8
r.ivanov <r.ivanov@corp.vk.com>
2021-08-05 01:50:12 +0300

    some functions

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,6 +2,27 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
     public static void main(String[] args) {
-        System.out.println("hello, world!");
+        hello("world");
+        hello("мир");
+
+        double len = 5;
+        System.out.println("площадь квадрата со стороной " + len + "= " + area(len));
+
+        double a = 2;
+        double b = 3;
+        System.out.println("площадь прямоугольника со сторонами " + a + " и "+ b + " равна " + areaSquare(a,b));
     }
+
+    public static void hello(String somebody) {
+
+        System.out.println("hello " + somebody);
+    }
+
+    public static double area(double l) {
+        return l * l;
+    }
+
+    public static double areaSquare(double a, double b){
+        return a * b;
+    };
 }
