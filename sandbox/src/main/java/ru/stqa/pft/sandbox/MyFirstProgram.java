2d6af2d1d6ffd6df791bb66f8833b65291d7d1a7
r.ivanov <r.ivanov@corp.vk.com>
2021-08-05 00:48:03 +0300

    empty

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,7 +1,11 @@
 package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
-	public static void main(String[] args){
-		System.out.println("hello, world!");
-	}
+    public static void main(String[] args) {
+        System.out.println("hello, world!");
+
+		System.out.println(2+2);
+        System.out.println(2*2);
+        System.out.println(2.0000/2);
+    }
 }
